167d192053fc7863aedd21c2034654246fab99e3
rxhunter00 <134472915+rxhunter00@users.noreply.github.com>
2024-04-25 11:32:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,9 @@
 
 <p>Hello user, your current group is beginner [ <?php echo $_GET['group']; ?> ] </p>
 
+<p>Hello user, your current group is beginner [ <?php echo htmlspecialchars($_GET['group'], ENT_QUOTES , 'UTF-8'); ?> ] </p>
+
+<p>Hello user, your current group is [ {{ $_GET['group'] }} ] </p>
+
 </body>
 </html>

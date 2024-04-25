e892bbe933411fa50763aa7c75d9d21f67faa283
rxhunter00 <134472915+rxhunter00@users.noreply.github.com>
2024-04-25 11:35:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+  <title>Test XSS</title>
+</head>
 <body>
 
 <p>Hello user, your current group is beginner [ <?php echo htmlspecialchars($_GET['group'], ENT_QUOTES , 'UTF-8'); ?> ] </p>
